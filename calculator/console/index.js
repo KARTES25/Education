f0b10fd722e972d9f370c604a0850a21d5b163ce
KARTES <stilalex@yandex.ru>
2019-08-25 16:34:24 +0700

    end

---
+++ calculator/console/index.js
@@ -0,0 +1,7 @@
+		var left = prompt("первое число");
+		var action = prompt("действие");
+		var value = prompt("второе число число");
+			if (action=="-") {alert(left-value)};
+			if (action=="+") {alert(left+value)};
+			if (action=="*") {alert(left*value)};
+			if (action=="/") {alert(left/value)};
