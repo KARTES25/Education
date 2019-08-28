1ab06db73f9d5503b4444bf0c1cfa8e1db177920
KARTES <stilalex@yandex.ru>
2019-08-28 22:06:47 +0700

    add js code

--- calculator/console/index.js
+++ calculator/console/index.js
@@ -1,7 +1,72 @@
-		var left = prompt("первое число");
-		var action = prompt("действие");
-		var value = prompt("второе число число");
-			if (action=="-") {alert(left-value)};
-			if (action=="+") {alert(left+value)};
-			if (action=="*") {alert(left*value)};
-			if (action=="/") {alert(left/value)};
+let left = 0;
+let isLeft = true;
+let isCalced = false;
+let output = '';
+let action = '';
+
+function onResetButtonClick()
+{
+	output = '';
+    left = 0;
+    isLeft = true;
+};
+
+function onCalcButtonClick()
+{
+    let value = parseFloat(output);
+    let result = 0;
+
+    console.log('value', value)
+
+    if (action == "+") {
+        result = left + value;
+    } else
+    if (action == "-") {
+        result = left - value;
+    } else
+    if (action == "x") {
+        result = left * value;
+    } else
+    if (action == "/") {
+        result = left / value;
+    }
+	console.log('result', result);
+    output = String(result);
+
+	isCalced = true;
+    isLeft = true;
+    left = value;
+}
+
+function onNumButtonClick(digit){
+	let currentValue = output;
+	console.log('number clicked', digit)
+
+	if (!isLeft || isCalced) {
+        console.log("reset output");
+        currentValue = "";
+    }
+
+    isLeft = true;
+
+    if (isCalced) {
+        console.log("reset left");
+        left = 0;
+    }
+
+    isCalced = false;
+
+    output = currentValue + digit;
+}
+
+function onActionButtonClick(inputAction)
+{
+   	let value = parseFloat(output);
+    action = inputAction;
+    left = value;
+    isLeft = false;
+    isCalced = false;
+
+    console.log("action", action);
+	console.log("left", value);
+}
